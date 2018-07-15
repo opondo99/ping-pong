4f6f260a914f1da2324b983b5cee6084d7e80976
opondo99 <markopondo.m@gmail.com>
2018-07-15 14:03:42 +0300

    added changes to script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 $(document).ready(function(){
-    $("form#userInput").submit(function(event){
+    $("form#userInputForm").submit(function(event){
         event.preventDefault()
+        var input = parseInt($("input#userInput").val());
+
+        for(var number = 1; number <= input; number++){
+            if((number %3 === 0) && (number %5 === 0))
+                $("#result").text("pingpong");
+            else if(number %3 === 0)
+                $("#result").text("ping");
+            else if(number %5 === 0)
+                $("#result").text("pong");
+            else
+                $("#result").text(number);
+        }
     })
-    var input = parseInt($("input#userInput").val());
-    $("#result").text(input)
+
 });
 
 
-for(var input = 0; input <= 100; input++){
-    if((input%3 === 0) && (input%5 === 0))
-        console.log("pingpong");
-    else if(input%3 === 0)
-        console.log("ping");
-    else if(input%5 === 0)
-        console.log("pong");
-    else
-        console.log(input);
-}
+
     
 
-
+    
